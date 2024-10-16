418b223fa205d9f4928b704db0819b6520a88bba
Hamza Faqhi <Humzafaqi@gmail.com>
2024-10-16 13:30:21 +0500

    cleaning

--- src/components/cards/PreviewImageModal.jsx
+++ src/components/cards/PreviewImageModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import "./css/PreviewImageModal.css";
 
 const PreviewImageModal = ({ show, handleClose, enlargedImageSrc, title }) => {
